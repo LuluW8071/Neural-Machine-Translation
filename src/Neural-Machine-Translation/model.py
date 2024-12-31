3992075b56ecd024bdfa91aaab010301950c7d1e
luluw <shashinmhrzn@gmail.com>
2024-12-31 12:59:38 +0545

    :rocket: [ADD] Base GRU Combined Enc-Dec

---
+++ src/Neural-Machine-Translation/model.py
@@ -0,0 +1,18 @@
+import torch.nn as nn
+from models.gru import Encoder, Decoder
+
+class NMTModel(nn.Module):
+    def __init__(self, input_size, output_size, hidden_size, num_layers, max_len, dropout_rate=0.1):
+        super(NMTModel, self).__init__()
+        self.encoder = Encoder(input_size, hidden_size, num_layers, dropout_rate)
+        self.decoder = Decoder(hidden_size, output_size, num_layers)
+        self.max_len = max_len
+
+    def forward(self, input_tensor, target_tensor=None):
+        # Encoder forward pass
+        encoder_out, encoder_hidden = self.encoder(input_tensor)
+
+        # Decoder forward pass
+        decoder_outputs, decoder_hidden = self.decoder(encoder_out, encoder_hidden, self.max_len, target_tensor)
+
+        return decoder_outputs, decoder_hidden
