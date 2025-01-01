d2b1fd932752d1e8e47b4f7b6447d77458880aed
luluw <shashinmhrzn@gmail.com>
2025-01-01 20:34:01 +0545

    :rocket: [Update] BiDirectional Choice in Encoder Model

--- src/Neural-Machine-Translation/models/gru.py
+++ src/Neural-Machine-Translation/models/gru.py
@@ -4,56 +4,63 @@ import torch.nn.functional as F
 
 
 class Encoder(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, dropout_rate=0.1):
+    def __init__(self, input_size, hidden_size, num_layers, bidirection=True, dropout_rate=0.1):
         super(Encoder, self).__init__()
         self.hidden_size = hidden_size
         self.num_layers = num_layers
+        self.bidirectional = bidirection
         self.embedding = nn.Embedding(input_size, hidden_size)
         
-        # Used Bidiretional to capture the full context of the input sequence, enriching token representations by considering both past and future information
-        self.gru = nn.GRU(hidden_size, hidden_size, num_layers,
-                          bidirectional=False,
-                        #   batch_first=True,
+        self.gru = nn.GRU(hidden_size, 
+                          hidden_size, 
+                          num_layers,
+                          bidirectional=bidirection,
+                          batch_first=True,
                           dropout=dropout_rate)
+        
         self.dropout = nn.Dropout(dropout_rate)
-        self.fc = nn.Linear(self.hidden_size, self.hidden_size)
+        self.fc = nn.Linear(self.hidden_size*2, self.hidden_size)   # For Bidirectional
 
     def forward(self, input):
         embedded = self.dropout(self.embedding(input))
         out, hidden = self.gru(embedded)
+        
+        if self.bidirectional:
+            # Combine forward and backward hidden states across all layers
+            hidden = torch.cat((hidden[0:self.num_layers], hidden[self.num_layers:]), dim=2)
+            hidden = self.fc(hidden)
 
-        # Combine forward and backward hidden states across all layers
-        # hidden = torch.cat((hidden[0:1], hidden[1:2]), dim=2)
-        # hidden = self.fc(torch.cat((hidden[0:1], hidden[1:2]), dim=1))
-        print("Encoder Hidden", hidden.shape)
         return out, hidden
 
 
 class Decoder(nn.Module):
-    def __init__(self, hidden_size, output_size, num_layers):
+    def __init__(self, hidden_size, output_size, num_layers, device):
         super(Decoder, self).__init__()
         self.embedding = nn.Embedding(output_size, hidden_size)
 
-        # In NMT Bidirectional RNN Decoder doesn't make sense
-        # as decoding is autoregressive process; each token is generated step-by-step, donidtioned on prev. tokens and encoder outputs.
-        self.gru = nn.GRU(hidden_size, hidden_size,
-                          num_layers, bidirectional=False, batch_first=True)
+        # NOTE: In NMT Bidirectional RNN Decoder doesn't make sense
+        # as decoding is autoregressive process; each token is generated step-by-step, conidtioned on prev. tokens and encoder outputs.
+        self.gru = nn.GRU(hidden_size, 
+                          hidden_size,
+                          num_layers, 
+                          bidirectional=False, 
+                          batch_first=True)
+        
         self.out = nn.Linear(hidden_size, output_size)
+        self.device = device
 
     def forward(self, encoder_out, encoder_hidden, max_len, target_tensor=None):
         # Get batch size of encoder output
-        bs = encoder_out.size(1)
-        # print("batch:", bs)
+        bs = encoder_out.size(0)
 
         # Initial decoder input (SOS token: 0)
-        decoder_input = torch.empty(bs, 1, dtype=torch.long).fill_(0)   
+        decoder_input = torch.empty(bs, 1, dtype=torch.long).fill_(0).to(self.device)  
 
+        # Match decoder hidden state to encoder hidden state
         decoder_hidden = encoder_hidden
 
+        # List to hold decoder outputs
         decoder_outputs = []
-
-        # print(decoder_input.shape, decoder_hidden.shape)
-
         for i in range(max_len):
             decoder_output, decoder_hidden = self.forward_step(decoder_input, decoder_hidden)
             decoder_outputs.append(decoder_output)
@@ -71,7 +78,6 @@ class Decoder(nn.Module):
 
     def forward_step(self, decoder_input, decoder_hidden):
         output = self.embedding(decoder_input)
-        # print("Decoder Embeding Output:", output.shape) 
         output = F.relu(output)
         output, hidden = self.gru(output, decoder_hidden)
         output = self.out(output)
