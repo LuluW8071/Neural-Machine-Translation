868c5bbcba6f75673f9f01c3bd64bca7add228c5
luluw <shashinmhrzn@gmail.com>
2025-01-06 17:12:21 +0545

    :rocket: [Update] Base Model Modularized

--- src/Neural-Machine-Translation/model.py
+++ src/Neural-Machine-Translation/model.py
@@ -1,12 +1,18 @@
 import torch.nn as nn
-from models.gru import Encoder, Decoder
+from models.encoder import Encoder
+from models.decoder import Decoder
+from models.attn_decoder import AttnDecoder
 
 
 class NMTModel(nn.Module):
-    def __init__(self, input_size, output_size, hidden_size, num_layers, max_len, bidirection=True, dropout_rate=0.1, device="cpu"):
+    def __init__(self, input_size, output_size, hidden_size, num_layers, max_len, bidirection=True, dropout_rate=0.1, attention=True, device="cpu"):
         super(NMTModel, self).__init__()
         self.encoder = Encoder(input_size, hidden_size, num_layers, bidirection, dropout_rate)
-        self.decoder = Decoder(hidden_size, output_size, num_layers, device)
+        self.decoder = (
+            AttnDecoder(hidden_size, output_size, num_layers, device)
+            if attention
+            else Decoder(hidden_size, output_size, num_layers, device)
+        )
         self.max_len = max_len
 
     def forward(self, input_tensor, target_tensor=None):
@@ -14,6 +20,6 @@ class NMTModel(nn.Module):
         encoder_out, encoder_hidden = self.encoder(input_tensor)
 
         # Decoder forward pass
-        decoder_outputs, decoder_hidden, _ = self.decoder(encoder_out, encoder_hidden, self.max_len, target_tensor)
+        decoder_outputs, decoder_hidden, attention = self.decoder(encoder_out, encoder_hidden, self.max_len, target_tensor)
 
-        return decoder_outputs, decoder_hidden
+        return decoder_outputs, decoder_hidden, attention
